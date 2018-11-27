471519fcdc61ecdb0110b5148728173c87a27d54
Jhofran Briceño <jjbriceno3@misena.edu.co>
2018-11-27 15:54:26 -0500

    versin 0.1.1v

---
+++ app/src/main/java/co/sena/amasplay/myturn/Perfil_Usuario_Activity.java
@@ -0,0 +1,13 @@
+package co.sena.amasplay.myturn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perfil_Usuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil__usuario_);
+    }
+}
